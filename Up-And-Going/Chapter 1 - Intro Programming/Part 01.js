a2fbb1cd05c54d78ae78c6b8dbd2e66f141e08a7
ChelseyJan <chelseyjan@gmail.com>
2017-02-28 01:36:36 -0800

    1st half of Chapter 1 - JS

---
+++ Up-And-Going/Chapter 1 - Intro Programming/Part 01.js
@@ -0,0 +1,54 @@
+//Each example is divided by a comment.
+//Run lines of code all at once when between a comment.
+
+a = 21;
+
+b = a * 2;
+
+console.log(b);
+
+//Difference between console.log and alert
+
+a = 21;
+
+b = a * 2;
+
+alert(b);
+
+// Using basic input in JS
+
+age = prompt("Please tell me your age:" );
+
+console.log(age);
+
+// Variable Creation
+
+var a = 20;
+
+a = a + 1;
+a = a * 2;
+
+console.log (a);
+
+//Coercion: converting string to a number
+
+var a = "42"; 
+var b = Number(a);
+
+console.log (a);
+console.log (b);
+
+// Changing a number to a string
+
+var amount = 99.99;
+
+amount = amount * 2;
+
+console.log(amount);
+
+amount = "$" + String(amount);
+
+console.log(amount);
+
+//
+
