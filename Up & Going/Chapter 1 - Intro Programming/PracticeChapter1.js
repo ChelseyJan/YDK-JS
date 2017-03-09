58e53480d7d9d0fe81d15de1908c177f7296ebad
ChelseyJan <chelseyjan@gmail.com>
2017-03-08 21:44:22 -0800

    Yeah

--- Up & Going/Chapter 1 - Intro Programming/PracticeChapter1.js
+++ Up & Going/Chapter 1 - Intro Programming/PracticeChapter1.js
@@ -9,24 +9,44 @@ https://github.com/getify/You-Dont-Know-JS */
 // The following program is used to calculate the total cost of a phone purchase
 
 //Calculate the total price of a phone purchase
-//Keep purchasing phones (as a loop) until you run out of money in your bank account
+
 //Buy accessories for each phone as long as your purchase amount is below your mental spending threshold.
 //After caculating purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted.
 //Check amount against bank account balance to see if you can afford it.
+//Incorporate input into this program, perhaps with prompt(..)
 
 // Declare constants
-var TAX_RATE;
-var PHONE_PRICE;
-var ACC_PRICE;
-var SPEND_THRESHOLD;
+var TAX_RATE = 0.08;
+var ACC_PRICE = 9.99;
+var SPEND_THRESHOLD = 350.00;
 
 // Declare variables
-var bankAcctBal;
+var phonePrice = 0;
+var bankAcctBal = 0;
 
-// Function to properly format the price for display
+// Function to properly format the output of a price
 function printPrice(price) {
-	price = price + 0.0041; // adding 0.0041 guarantees a round up instead of round down (currency)
-	//console.log(price); // debug
-	price = price.toFixed(2); // fix number decimal point, convert to string
+	price = price + 0.0041; // adding 0.0041 guarantees a round up instead of round down (currency reasons)
+	price = price.toFixed(2);
 	console.log("$" + price);
 }
+
+//Keep purchasing phones (as a loop) until you run out of money in your bank account
+phonePrice = 99.99
+bankAcctBal = 350.00
+function buyPhones() {
+	var subtotal = 0;
+	while (bal < bankAcctBal) {
+		subtotal = subtotal + phonePrice;
+	}
+	return subtotal;
+}
+
+var subtotal = 0;
+subtotal = buyPhones;
+console.log(subtotal);
+// Function to purchase phones until out of money in bank account
+
+bankAcctBal = prompt("What is the current balance in your bank account?");
+
+phonePrice = prompt("What is the cost of the phone you will be buying?");
